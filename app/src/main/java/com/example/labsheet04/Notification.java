fa6258d409e33aa68b01a0e775e18539acaf29b2
NithyaYamsinghe <nitheeromeshi@gmail.com>
2020-03-27 17:51:20 +0530

    first commit

---
+++ app/src/main/java/com/example/labsheet04/Notification.java
@@ -0,0 +1,15 @@
+package com.example.labsheet04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// IT18233704 N.R Yamasinghe
+public class Notification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
